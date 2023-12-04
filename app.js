910b6b7b9255e4093b635072e144fe55e9e76308
Erick Valencia <evalencia0911@gmail.com>
2023-12-04 15:33:37 -0700

    commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More Section

--- app.js
+++ app.js
@@ -18,26 +18,69 @@ $(() => {
     const $h2 = $("<h2>");
     //console.log($h2)
     $h2.text("Erick Valencia");
-    $container.append($h2)
+    $container.append($h2);
 
     //h3 element with your house
     const $h3 = $("<h3>");
     //console.log($h3)
-    $h3.text("Slytherin")
+    $h3.text("Slytherin");
     $container.append($h3);
 
     //h4 element with your pet's name
-    const $h4a = $("<h4>")
+    const $h4a = $("<h4>");
     //console.log($h4)
     //add an attribute of fon-tfamily: fantasy; to all elements that have a class tha matches your pet's class
-    $h4a.attr("id", '#puppie')
-    $h4a.text("Pippin")
-    $container.append($h4a)
+    $h4a.attr("id", '#puppie');
+    $h4a.text("Pippin");
+    $container.append($h4a);
 
-    const $h4b = $("<h4>")
+    const $h4b = $("<h4>");
     //console.log($h4b)
-    $h4b.text("Wand of the Puppie Pal")
-    $container.append($h4b)
+    $h4b.text("Wand of the Puppie Pal");
+    $container.append($h4b);
+
+    //create a ul of the below with the attribute of storage and value of trunk
+
+    const $storageTrunkUl = $("<ul>");
+    //console.log($storageTrunkUl)
+    $storageTrunkUl.attr("storage", "trunk");
+    $container.append($storageTrunkUl)
+
+    //list of items
+
+    //butter beer
+    const $butterBeerLi = $("<li>").text("Butter Beer")
+    //console.log($butterBeerLi)
+    $storageTrunkUl.append($butterBeerLi)
+
+    //invisibility cloak (add a class of secret)
+    const $invisibilityCloakLi = $("<li>").text("Invisibility Cloak")
+    //console.log($invisibilityCloakLi)
+    $invisibilityCloakLi.attr("class", "secret")
+    $storageTrunkUl.append($invisibilityCloakLi)
+
+    //magic map (add a class of secret)
+    const $magicMapLi = $("<li>").text("Magic Map")
+    //console.log($magicMapLi)
+    $magicMapLi.attr("class", "secret")
+    $storageTrunkUl.append($magicMapLi)
+
+    //time turner (add a class of secret)
+    const $timeTurnerLi = $("<li>").text("Time Turner")
+    //console.log($timeTurnerLi)
+    $timeTurnerLi.attr("class", "secret")
+    $storageTrunkUl.append($timeTurnerLi)
+
+    //Leash (ensure you give the element the same class as your pet)
+    const $leashLi = $("<li>").text("Leash")
+    //console.log($leashLi)
+    $leashLi.attr("id", "#puppie")
+    $storageTrunkUl.append($leashLi)
+
+    //Bertie Bott' Every Flavor [Jelly] Beans
+    const $beansLi = $("<li>").text("Bertie Bott's Every Flavor [Jelly] Beans.")
+    //console.log($beansLi)
+    $storageTrunkUl.append($beansLi)
 });
 
 //Query for your div with the id of container and set it to a variable named $container
